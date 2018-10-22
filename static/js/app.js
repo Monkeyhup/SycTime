e76ee1202a16cc45cd9ce8e0d5e17d05df0100fd
monkey <guomengjie7@163.com>
2018-10-22 18:53:34 +0800

    精简代码

--- static/js/app.js
+++ static/js/app.js
@@ -11,103 +11,103 @@ $(function () {
         "ts":[],
         "bj":[],
         "db":[]
-    }
+    };
     let socket = io();
     socket.on('bupt', function (data) {
         if (datum.bupt.length > 1000) {
-            datum.bupt.unshift()
+            datum.bupt.unshift();
         }
         datum.bupt.push(data);
-        $("#txt1").text(`${data[0]}:${data[1]}:${data[2]}.${data[3]}`)
-    })
+        $("#txt1").text(`${data[0]}:${data[1]}:${data[2]}.${data[3]}`);
+    });
     socket.on('jly', function (data) {
         if (datum.jly.length > 1000) {
-            datum.jly.unshift()
+            datum.jly.unshift();
         }
         datum.jly.push(data);
-        $("#txt2").text(`${data[0]}:${data[1]}:${data[2]}.${data[3]}`)
-    })
+        $("#txt2").text(`${data[0]}:${data[1]}:${data[2]}.${data[3]}`);
+    });
     socket.on('usa', function (data) {
         if (datum.usa.length > 1000) {
-            datum.usa.unshift()
+            datum.usa.unshift();
         }
         datum.usa.push(data);
-        $("#txt3").text(`${data[0]}:${data[1]}:${data[2]}.${data[3]}`)
-    })
+        $("#txt3").text(`${data[0]}:${data[1]}:${data[2]}.${data[3]}`);
+    });
     socket.on('China', function (data) {
         if (datum.China.length > 1000) {
-            datum.China.unshift()
+            datum.China.unshift();
         }
         datum.China.push(data);
-        $("#txt4").text(`${data[0]}:${data[1]}:${data[2]}.${data[3]}`)
-    })
+        $("#txt4").text(`${data[0]}:${data[1]}:${data[2]}.${data[3]}`);
+    });
     socket.on('Apple', function (data) {
         if (datum.Apple.length > 1000) {
-            datum.Apple.unshift()
+            datum.Apple.unshift();
         }
         datum.Apple.push(data);
-        $("#txt5").text(`${data[0]}:${data[1]}:${data[2]}.${data[3]}`)
-    })
+        $("#txt5").text(`${data[0]}:${data[1]}:${data[2]}.${data[3]}`);
+    });
     socket.on('mcc', function (data) {
         if (datum.mcc.length > 1000) {
-            datum.mcc.unshift()
+            datum.mcc.unshift();
         }
         datum.mcc.push(data);
-        $("#txt6").text(`${data[0]}:${data[1]}:${data[2]}.${data[3]}`)
-    })
+        $("#txt6").text(`${data[0]}:${data[1]}:${data[2]}.${data[3]}`);
+    });
     socket.on('ali', function (data) {
         if (datum.ali.length > 1000) {
-            datum.ali.unshift()
+            datum.ali.unshift();
         }
         datum.ali.push(data);
-        $("#txt7").text(`${data[0]}:${data[1]}:${data[2]}.${data[3]}`)
-    })
+        $("#txt7").text(`${data[0]}:${data[1]}:${data[2]}.${data[3]}`);
+    });
     socket.on('ts', function (data) {
         if (datum.ts.length > 1000) {
-            datum.ts.unshift()
+            datum.ts.unshift();
         }
         datum.ts.push(data);
-        $("#txt8").text(`${data[0]}:${data[1]}:${data[2]}.${data[3]}`)
-    })
+        $("#txt8").text(`${data[0]}:${data[1]}:${data[2]}.${data[3]}`);
+    });
     socket.on('bj', function (data) {
         if (datum.bj.length > 1000) {
-            datum.bj.unshift()
+            datum.bj.unshift();
         }
         datum.bj.push(data);
-        $("#txt9").text(`${data[0]}:${data[1]}:${data[2]}.${data[3]}`)
-    })
+        $("#txt9").text(`${data[0]}:${data[1]}:${data[2]}.${data[3]}`);
+    });
     socket.on('db', function (data) {
         if (datum.db.length > 1000) {
-            datum.db.unshift()
+            datum.db.unshift();
         }
         datum.db.push(data);
-        $("#txt10").text(`${data[0]}:${data[1]}:${data[2]}.${data[3]}`)
-    })
+        $("#txt10").text(`${data[0]}:${data[1]}:${data[2]}.${data[3]}`);
+    });
     $("#btn").on('click', function () {
         let source = {};
         let arrCheckbox = $("input[name='check']");
         for (i = 0; i < arrCheckbox.length; i++) {
             if (arrCheckbox[i].checked) {
-                source[arrCheckbox[i].value]=datum[arrCheckbox[i].value]
+                source[arrCheckbox[i].value]=datum[arrCheckbox[i].value];
             }
         }
-        console.log(source)
-    })
+        console.log(source);
+    });
     var groupCheckbox = $("input[name='check']");
     // var groupCheckbox = $(".option");
     for (i = 0; i < groupCheckbox.length; i++) {
-        console.log(groupCheckbox)
-        groupCheckbox[i].onclick = function () {
+        console.log(groupCheckbox);
+        groupCheckbox[i].onclick =  ()=> {
             console.log(this.checked);
             if (this.checked) {
-                this.parentNode.setAttribute("class", "ac")
+                this.parentNode.setAttribute("class", "ac");
             } else {
-                this.parentNode.setAttribute("class", "qu")
+                this.parentNode.setAttribute("class", "qu");
             }
-        }
+        };
     }
 
-    drawTable()
+    drawTable();
 });
 
 
@@ -124,7 +124,7 @@ function checkTime(i) {
  * 为毫秒加0
  * */
 function checkMsTime(i) {
-    console.log('star')
+    console.log('star');
     if (i.length == 1) {
         i = "00" + i;
     } else if (i.length == 2) {
@@ -134,6 +134,6 @@ function checkMsTime(i) {
 }
 
 function dian(fou) {
-    console.log(fou)
-    console.log(fou.checked)
+    console.log(fou);
+    console.log(fou.checked);
 }
--- test.js
+++ test.js
@@ -35,28 +35,7 @@ function getLocalTime(msTime) {
         return str;
     }
 }
-/**
- * 减法运算，避免数据相减小数点后产生多位数和计算精度损失。
- * 
- * @param num1被减数  |  num2减数
- */
-function numSub(num1, num2) {
-	var baseNum, baseNum1, baseNum2;
-	var precision;// 精度
-	try {
-		baseNum1 = num1.toString().split(".")[1].length;
-	} catch (e) {
-		baseNum1 = 0;
-	}
-	try {
-		baseNum2 = num2.toString().split(".")[1].length;
-	} catch (e) {
-		baseNum2 = 0;
-	}
-	baseNum = Math.pow(10, Math.max(baseNum1, baseNum2));
-	precision = (baseNum1 >= baseNum2) ? baseNum1 : baseNum2;
-	return ((num1 * baseNum - num2 * baseNum) / baseNum).toFixed(precision);
-};
+
 let a = 10 - 'timeout';
 let b = Number((10 - 'timeout').toFixed(2));
 let c = parseInt(5376524724.1561);
@@ -67,5 +46,5 @@ let f=numSub(3748902134720.5996,3748902135722.561);
 console.log(3748902134720.5996-3748902135722.561);
 console.log((134720.5996-'time').toFixed(3));
 console.log(typeof Number((134720.5996-'time').toFixed(3)));
-console.log(f)
+console.log(f);
 
